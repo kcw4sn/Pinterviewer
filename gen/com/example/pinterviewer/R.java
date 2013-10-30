51335648d64c9ee287d926c18422ede5f3b30e41
The Spectra <thespectra@d-172-25-98-236.bootp.virginia.edu>
2013-10-30 16:07:04 -0400

    End of Phase 2, successful android app

--- gen/com/example/pinterviewer/R.java
+++ gen/com/example/pinterviewer/R.java
@@ -24,9 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
+        public static final int action_settings=0x7f080008;
         public static final int createAccountButton=0x7f080000;
         public static final int loginButton=0x7f080001;
+        public static final int outputLabel=0x7f080007;
         public static final int passwordField=0x7f080005;
         public static final int passwordLabel=0x7f080004;
         public static final int usernameField=0x7f080002;
@@ -44,6 +45,7 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int createAccountStaticString=0x7f050005;
         public static final int loginStaticString=0x7f050004;
+        public static final int outputLabel=0x7f050007;
         public static final int passwordStaticString=0x7f050003;
         public static final int usernameStaticString=0x7f050002;
         public static final int welcomeMessage=0x7f050006;
--- src/com/example/pinterviewer/Pinterviewer.java
+++ src/com/example/pinterviewer/Pinterviewer.java
@@ -3,11 +3,15 @@ package com.example.pinterviewer;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
@@ -16,6 +20,7 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -23,35 +28,41 @@ import org.json.JSONTokener;
 
 import com.example.pinterviewer.R;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.res.Resources.NotFoundException;
 import android.view.Menu;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.view.View.OnClickListener;
 
 public class Pinterviewer extends Activity implements OnClickListener{
 
 	private Button loginButton, createAccountButton;
 	private EditText usernameField, passwordField;
-	
+	private TextView outputLabel;
+
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pinterviewer);
-        
-        loginButton = (Button) findViewById(R.id.loginButton);
-        createAccountButton = (Button) findViewById(R.id.createAccountButton);
-        loginButton.setOnClickListener(this);
-        createAccountButton.setOnClickListener(this);
-        
-        usernameField = (EditText) findViewById(R.id.usernameField);
-        passwordField = (EditText) findViewById(R.id.passwordField);
-        
-        //addListenerOnButtons();
-    }
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_pinterviewer);
+
+		loginButton = (Button) findViewById(R.id.loginButton);
+		createAccountButton = (Button) findViewById(R.id.createAccountButton);
+		loginButton.setOnClickListener(this);
+		createAccountButton.setOnClickListener(this);
+
+		usernameField = (EditText) findViewById(R.id.usernameField);
+		passwordField = (EditText) findViewById(R.id.passwordField);
+		outputLabel = (TextView) findViewById(R.id.outputLabel);
+
+
+		//addListenerOnButtons();
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -59,85 +70,123 @@ public class Pinterviewer extends Activity implements OnClickListener{
 		getMenuInflater().inflate(R.menu.pinterviewer, menu);
 		return true;
 	}
-	
+
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		switch (v.getId()) {
 		case R.id.loginButton:
-				usernameField.setText(usernameField.getText());
-				passwordField.setText(passwordField.getText());
-
-				postData();
+			usernameField.setText(usernameField.getText());
+			passwordField.setText(passwordField.getText());
+			Log.d("Web", "Login button click detected");	 
+			new PostData().execute("");
 			break;
 		case R.id.createAccountButton:
 
-				postData();
+			//new LongOperation().execute("");
 			break;
 
 		default:
 			break;
 		}
 	}
-	
-	
-	//Adapted from http://www.androidsnippets.com/executing-a-http-post-request-with-httpclient
-	public void postData() {
-	    // Create a new HttpClient and Post Header
-	    HttpClient httpclient = new DefaultHttpClient();
-		//HttpURLConnection httpclient = null; //new DefaultHttpURLConnection();
-	    HttpPost httppost = new HttpPost("http://plato.cs.virginia.edu/~cs4720f13yam/pinterviewWebApp/");
-
-	    try {
-	    	// Add your data
-	        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-	        nameValuePairs.add(new BasicNameValuePair("username", ""+usernameField.getText()));
-	        nameValuePairs.add(new BasicNameValuePair("password", ""+passwordField.getText()));
-	        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-	        //Log.d("Web", ""+ nameValuePairs.toString());
-
-	        // Execute HTTP Post Request
-	        HttpResponse response_json = httpclient.execute(httppost);
-	        
-Log.d("Web", "GOT TO THE STATEMENT");	 
 
 
-/*BufferedReader rd = new BufferedReader(new InputStreamReader(response_json.getEntity().getContent()));
+	String successString;
+	private class PostData extends AsyncTask<String, Void, String> {
 
-String line = "";
-while ((line = rd.readLine()) != null) {
-  System.out.println(line);
-  if (line.startsWith("Auth=")) {
-    String key = line.substring(5);
-    // do something with the key
-  }*/
+		@Override
+		protected String doInBackground(String... params) {
+			//ArrayList<NameValuePair> nvPairs = params[0];
+
+			// Create a new HttpClient and Post Header
+			HttpClient httpclient = new DefaultHttpClient();
+			try {
+				URI uri = new URI("http://plato.cs.virginia.edu/~cs4720f13yam/pinterviewWebApp/");
+				HttpPost httppost = new HttpPost(uri);
+				Log.d("Web", "HttpPost creation was successful");	
 
 
 
 
 
+				try {
+					// Add your data
+					List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+					nameValuePairs.add(new BasicNameValuePair("username", ""+usernameField.getText()));
+					nameValuePairs.add(new BasicNameValuePair("password", ""+passwordField.getText()));
+
+					httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+					Log.d("Web", "HttpPost URL encoded entity: " + EntityUtils.toString(httppost.getEntity()));	
+					Log.d("Web", "URL accessed: "+httppost.getURI());
+					// Execute HTTP Post Request
+					HttpResponse response = httpclient.execute(httppost);
+					Log.d("Web", "HttpPost executed and HttpResponse retrieved successfully! ");	        
+
+					HttpEntity entity = response.getEntity();
+					InputStream input_stream = entity.getContent();
+					Log.d("Web", "Input stream opened successfully");	 
+
+					//For testing
+					//InputStream input_stream = null;
+					//JSONArray finalResult = new JSONArray();
 
 
-	        /*// Parse the response        
-	        BufferedReader reader = new BufferedReader(new InputStreamReader(response_json.getEntity().getContent(), "UTF-8"));
-	        StringBuilder builder = new StringBuilder();
-	        for (String line = null; (line = reader.readLine()) != null;) {
-	            builder.append(line).append("\n");
-	        }
-	        JSONTokener tokener = new JSONTokener(builder.toString());
-	        JSONArray finalResult = new JSONArray(tokener);
-	        String response_str = finalResult.toString();
-	        System.out.println(response_str);
-	        
-	    } catch (ClientProtocolException e) {
-	        // TODO Auto-generated catch block
-	    } catch (IOException e) {
-	        // TODO Auto-generated catch block
-	    } catch (JSONException e) {*/
-	    	// TODO Auto-generated catch block
-	    } catch (Exception e) {
-	    	
-	    }
-	} 
+					// Parse the response        
+					BufferedReader reader = new BufferedReader(new InputStreamReader(input_stream, "UTF-8"));
+					StringBuilder builder = new StringBuilder();
+					for (String line = null; (line = reader.readLine()) != null;) {
+						builder.append(line).append("\n");
+					}
+					JSONTokener tokener = new JSONTokener(builder.toString());
+					JSONArray finalResult = new JSONArray(tokener);
+					String response_str = finalResult.toString();
+
+					Log.d("Web", "JSON response stringified: " + response_str);	
+					successString = "Login was successful! JSON output was: " 
+							+ response_str 
+							+ ". For future implementation, this list will be displayed as links on a new page.";
+
+				} catch (ClientProtocolException e) {
+					Log.d("Web", "ClientProtocol exception thrown!");
+					successString = "ERROR";
+				} catch (IOException e) {
+					Log.d("Web", "IO exception thrown!");
+					successString = "ERROR";
+				} catch (JSONException e) {
+					Log.d("Web", "JSON exception thrown!");
+					successString = "ERROR";
+				} catch (NotFoundException e) {
+					Log.d("Web", "NotFound exception thrown!");
+					successString = "ERROR";
+				} catch (Exception e) {
+					Log.d("Web", "Generic exception thrown!");
+					successString = "ERROR";
+					e.printStackTrace();
+				}
+			} catch (URISyntaxException e1) {
+				// TODO Auto-generated catch block
+				Log.d("Web", "HttpPost creation fialed: Improper URL!");
+				e1.printStackTrace();
+				successString = "ERROR";
+			}
+
+			return successString;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			TextView txt = (TextView) findViewById(R.id.outputLabel);
+			txt.setText(result);
+			// might want to change "executed" for the returned string passed
+			// into onPostExecute() but that is upto you
+		}
+
+		@Override
+		protected void onPreExecute() {}
+
+		@Override
+		protected void onProgressUpdate(Void... values) {}
+	}
 
 }
