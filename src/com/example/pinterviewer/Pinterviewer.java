4613b297b0a3e4c70b29e645302c3bf5521d2a53
The Spectra <thespectra@The-Spectras-MacBook-Pro.local>
2013-10-29 21:35:11 -0400

    Status at the end of 10/29. Issues with parsing the POST data on the web service side, and unsure of how to pass the JSON result back to the app.

--- src/com/example/pinterviewer/Pinterviewer.java
+++ src/com/example/pinterviewer/Pinterviewer.java
@@ -4,6 +4,7 @@ package com.example.pinterviewer;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -43,10 +44,13 @@ public class Pinterviewer extends Activity implements OnClickListener{
         
         loginButton = (Button) findViewById(R.id.loginButton);
         createAccountButton = (Button) findViewById(R.id.createAccountButton);
+        loginButton.setOnClickListener(this);
+        createAccountButton.setOnClickListener(this);
+        
         usernameField = (EditText) findViewById(R.id.usernameField);
         passwordField = (EditText) findViewById(R.id.passwordField);
         
-        
+        //addListenerOnButtons();
     }
 
 	@Override
@@ -55,24 +59,19 @@ public class Pinterviewer extends Activity implements OnClickListener{
 		getMenuInflater().inflate(R.menu.pinterviewer, menu);
 		return true;
 	}
-
+	
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		switch (v.getId()) {
 		case R.id.loginButton:
-				//new AttemptLogin().execute();
 				usernameField.setText(usernameField.getText());
 				passwordField.setText(passwordField.getText());
-				//Log.i("Web", "button 1 pressed");
-				//Log.println(0, "Web", "button 1 pressed");
-				//Log.i("Web", usernameField.getText().toString());
+
 				postData();
 			break;
 		case R.id.createAccountButton:
-				//Log.i("button 2 pressed");
-				//Intent i = new Intent(this, Register.class);
-				//startActivity(i);
+
 				postData();
 			break;
 
@@ -86,19 +85,40 @@ public class Pinterviewer extends Activity implements OnClickListener{
 	public void postData() {
 	    // Create a new HttpClient and Post Header
 	    HttpClient httpclient = new DefaultHttpClient();
+		//HttpURLConnection httpclient = null; //new DefaultHttpURLConnection();
 	    HttpPost httppost = new HttpPost("http://plato.cs.virginia.edu/~cs4720f13yam/pinterviewWebApp/");
 
 	    try {
-	        // Add your data
+	    	// Add your data
 	        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
 	        nameValuePairs.add(new BasicNameValuePair("username", ""+usernameField.getText()));
 	        nameValuePairs.add(new BasicNameValuePair("password", ""+passwordField.getText()));
 	        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+	        //Log.d("Web", ""+ nameValuePairs.toString());
 
 	        // Execute HTTP Post Request
 	        HttpResponse response_json = httpclient.execute(httppost);
-	             
-	        // Parse the response        
+	        
+Log.d("Web", "GOT TO THE STATEMENT");	 
+
+
+/*BufferedReader rd = new BufferedReader(new InputStreamReader(response_json.getEntity().getContent()));
+
+String line = "";
+while ((line = rd.readLine()) != null) {
+  System.out.println(line);
+  if (line.startsWith("Auth=")) {
+    String key = line.substring(5);
+    // do something with the key
+  }*/
+
+
+
+
+
+
+
+	        /*// Parse the response        
 	        BufferedReader reader = new BufferedReader(new InputStreamReader(response_json.getEntity().getContent(), "UTF-8"));
 	        StringBuilder builder = new StringBuilder();
 	        for (String line = null; (line = reader.readLine()) != null;) {
@@ -107,13 +127,16 @@ public class Pinterviewer extends Activity implements OnClickListener{
 	        JSONTokener tokener = new JSONTokener(builder.toString());
 	        JSONArray finalResult = new JSONArray(tokener);
 	        String response_str = finalResult.toString();
+	        System.out.println(response_str);
 	        
 	    } catch (ClientProtocolException e) {
 	        // TODO Auto-generated catch block
 	    } catch (IOException e) {
 	        // TODO Auto-generated catch block
-	    } catch (JSONException e) {
+	    } catch (JSONException e) {*/
 	    	// TODO Auto-generated catch block
+	    } catch (Exception e) {
+	    	
 	    }
 	} 
 
